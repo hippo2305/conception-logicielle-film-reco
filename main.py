4d3af9706ff54ad482649d7dcb5dbbb6d16bf14e
hippo2305 <hippolytecotterot@gmail.com>
2026-01-19 15:29:09 +0100

    TP .env.local

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+from dotenv import load_dotenv
+
+
+class MAIN:
+    def load_env():
+        # Charge le fichier principal
+        load_dotenv()
+        # Charge un fichier local si présent
+        local_env_path = ".env.local"
+        if os.path.exists(local_env_path):
+            load_dotenv(dotenv_path=local_env_path, override=True)
+
+    def get_local_env():
+        local_env_path = ".env.local"
+
+        if os.path.exists(local_env_path):
+            load_dotenv(dotenv_path=local_env_path)
+
+        keyword_mask = [
+            "password",
+            "pwd",
+            "jeton",
+            "token",
+            "secret",
+            "key",
+            "cle",
+            "mdp",
+            "motdepasse",
+        ]
+
+        for key, value in os.environ.items():
+            if any(mask in key.lower() for mask in keyword_mask):
+                print(f"{key}=", end="")
+                for i in range(len(key)):
+                    print("*", end="")
+                print("")
+            else:
+                print(f"{key}={value}")
+
+
+if __name__ == "__main__":
+    print(MAIN.get_local_env())
