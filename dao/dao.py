da5f24f17addac44098a5b4b261375a6dd9e2cdb
hippo2305 <hippolytecotterot@gmail.com>
2026-02-02 16:12:22 +0100

    Finalisation DBConnection et DAO pour la base Postgres

--- dao/dao.py
+++ dao/dao.py
@@ -1,4 +1,4 @@
-import duckdb
+from db_connection import DBConnection
 
 
 class DAO:
@@ -8,79 +8,45 @@ class DAO:
         """
         self.ordre_suppr_tables = ["ACTEUR", "FILMS", "USER"]
         # Ordre logique de suppression pour respecter les contraintes FK
-        with duckdb.connect("data/local.db") as connection:
-            connection.sql("""
+        with DBConnection().connection as connection, connection.cursor() as cursor:
+            cursor.execute("""
                 CREATE SEQUENCE IF NOT EXISTS id_sequence START 1;
                 CREATE TABLE IF NOT EXISTS USER (
-                id INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
+                id_user INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
                 pseudo VARCHAR(255) NOT NULL UNIQUE,
                 mdp VARCHAR(255) NOT NULL
                 );
                 CREATE TABLE IF NOT EXISTS FILMS (
-                id INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
+                id_films INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
                 titre VARCHAR(255) NOT NULL,
                 realisateur VARCHAR(255) NOT NULL,
                 annee INT,
                 genre VARCHAR(255) NOT NULL,
                 UNIQUE(titre, realisateur)
                 );
-                CREATE TABLE IF NOT EXISTS ACTEUR (
-                id INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
+                CREATE TABLE IF NOT EXISTS ACTOR (
+                id_actor INTEGER PRIMARY KEY DEFAULT nextval('id_sequence'),
                 nom VARCHAR(255) NOT NULL,
                 prenom VARCHAR(255) NOT NULL,
+                age INTEGER,
                 UNIQUE(nom, prenom)
                 );
                 """)
-
-    def query_csv_file(self, path: str, select: str, where=None):
-        query = f"""
-            SELECT {select}
-            FROM read_csv('{path}')
-        """
-        if where is not None:
-            query += f"WHERE {where}"
-        res = duckdb.sql(query).fetchall()
-        return res
-
-    def get_csv_headers(self, path: str):
-        query = f"""
-            SELECT *
-            FROM read_csv('{path}',
-                header = false)
-            LIMIT 1;
-        """
-        res = duckdb.sql(query).fetchone()
-        return res
-
-    def add_database_from_csv(self, path: str, table: str):
-        columns = str(DAO().get_csv_headers(path)).replace("'", "")
-        with duckdb.connect("data/local.db") as connection:
-            for line in DAO().query_csv_file(path, "*"):
-                connection.sql(f"INSERT INTO {table} {columns} VALUES {line}")
-
-    def query_data_base(self, table: str, select: str, where=None):
-        query = f"""
-            SELECT {select}
-            FROM {table}
-        """
-        if where is not None:
-            query += f"WHERE {where}"
-        with duckdb.connect("data/local.db") as connection:
-            res = connection.sql(query).fetchall()
-        return res
 
     def _del_data_table(self, nom_table: str | None = None) -> str | None:
         """
         Vide la table donnée en argument en majuscule
         Si aucune table n'est spécifiée, toutes les tables de la DB sont vidées
         """
-        with duckdb.connect("data/local.db") as connection:
+        with DBConnection().connection as connection, connection.cursor() as cursor:
             if nom_table in self.ordre_suppr_table:
-                connection.sql(f"DELETE FROM {nom_table};")
+                cursor.execute(f"DELETE FROM {nom_table};")
+                connection.commit()
                 return "table vidée"
             if nom_table is None:
                 for nom_table in self.ordre_suppr_table:
-                    connection.sql(f"DELETE FROM {nom_table};")
+                    cursor.execute(f"DELETE FROM {nom_table};")
+                connection.commit()
                 return "tables vidées"
 
     def _drop_table(self, nom_table: str | None = None) -> str | None:
@@ -88,11 +54,13 @@ class DAO:
         Supprime la table donnée en argument en majuscule
         Si aucune table n'est spécifiée, toutes les tables de la DB sont supprimées
         """
-        with duckdb.connect("data/local.db") as connection:
+        with DBConnection().connection as connection, connection.cursor() as cursor:
             if nom_table in self.ordre_suppr_tables:
-                connection.sql(f"DROP TABLE IF EXISTS {nom_table} CASCADE;")
+                cursor.execute(f"DROP TABLE IF EXISTS {nom_table} CASCADE;")
+                connection.commit()
                 return "table supprimée"
             if nom_table is None:
                 for table in self.ordre_suppr_tables:
-                    connection.sql(f"DROP TABLE IF EXISTS {table} CASCADE;")
+                    cursor.execute(f"DROP TABLE IF EXISTS {table} CASCADE;")
+                connection.commit()
                 return "tables supprimées"
--- dao/db_connection.py
+++ dao/db_connection.py
@@ -2,31 +2,31 @@ import os
 
 import dotenv
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 from utils.singleton import Singleton
 
 
 class DBConnection(metaclass=Singleton):
     """
-    Technical class to open only one connection to the DB.
+    Classe de connexion à la base de données
+    Elle permet de n'ouvrir qu'une seule et unique connexion
     """
 
     def __init__(self):
-        dotenv.load_dotenv(override=True)
-        # Open the connection.
+        """Ouverture de la connexion"""
+        dotenv.load_dotenv()
+
         self.__connection = psycopg2.connect(
             host=os.environ["POSTGRES_HOST"],
             port=os.environ["POSTGRES_PORT"],
             database=os.environ["POSTGRES_DATABASE"],
             user=os.environ["POSTGRES_USER"],
-            password=os.environ["POSTGRES_PASSWORD"]
+            password=os.environ["POSTGRES_PASSWORD"],
+            options=f"-c search_path={os.environ['POSTGRES_SCHEMA']}",
+            cursor_factory=RealDictCursor,
         )
 
     @property
     def connection(self):
-        """
-        return the opened connection.
-
-        :return: the opened connection.
-        """
         return self.__connection
